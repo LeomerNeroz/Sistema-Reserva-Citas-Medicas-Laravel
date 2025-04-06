e58bbc388eaca585d26ed719dc92907d00151ca4
LeomerNeroz <leomerjose12@gmail.com>
2025-04-06 14:15:04 -0400

    favicon2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,6 @@
   <meta name="keywords" content="">
 
  
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   
   <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -19,7 +17,7 @@
 
  
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
